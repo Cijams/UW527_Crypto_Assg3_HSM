0d4afa9de24c7408d137b5159066c0bac397642b
Matthew Sell <mrsell@uw.edu>
2020-03-01 10:33:36 -0800

    Added method to get Key Value by Key ID

--- Server/complete/src/main/java/springboot/PersistenceService.java
+++ Server/complete/src/main/java/springboot/PersistenceService.java
@@ -5,11 +5,13 @@ import java.util.List;
 public interface PersistenceService {
 
 	/**
-	 * Create a new user and store to the database
-	 * @param newUser The new user to create
-	 * @return The current state of the User
+	 * Create a new Key for a specific User
+	 * @param userName The ID of the User that will own the new Key
+	 * @param keyId The ID of the new Key
+	 * @param keyValue The value of the new Key
+	 * @return The current state of the newly-created Key
 	 */
-	public User createUser( User newUser );
+	public Key createKey( String userName, String keyId, String keyValue );
 	
 	/**
 	 * Create a new user and store to the database
@@ -20,24 +22,25 @@ public interface PersistenceService {
 	public User createUser( String userName, String passwordHash );
 	
 	/**
-	 * Get all Users stored in the database
-	 * @return All Users
-	 */
-	public List< User > findAllUsers();
-	
-	/**
-	 * Get a specific User by username
-	 * @param userName The username of the User to obtain
-	 * @return The User with the matching username
-	 */
-	public User getUserByUsername( String userName );
-	
-	/**
-	 * Convenience method for saving User changes (uses same method as "createUser")
-	 * @param user The User to update
+	 * Create a new user and store to the database
+	 * @param newUser The new user to create
 	 * @return The current state of the User
 	 */
-	public User updateUser( User user );
+	public User createUser( User newUser );
+	
+	/**
+	 * Delete a Key
+	 * @param userName The ID of the User that "owns" the Key
+	 * @param keyId The ID of the Key to delete
+	 * @return The current state of the User
+	 */
+	public User deleteKey( String userName, String keyId );
+	
+	/**
+	 * Delete a Sequencer from the database
+	 * @param seqName The name of the Sequence
+	 */
+	public void deleteSequencer( String seqName );
 	
 	/**
 	 * Remove a User from the database
@@ -46,34 +49,10 @@ public interface PersistenceService {
 	public void deleteUser( User user );
 	
 	/**
-	 * Get the "master" Key from the database
-	 * @return The master Key
+	 * Get all Users stored in the database
+	 * @return All Users
 	 */
-	public Key getMasterKey();
-	
-	/**
-	 * Set the "master" Key in the database (there can only be one master key at a time)
-	 * @param value The value of the Key
-	 * @return The new master Key
-	 */
-	public Key setMasterKey( String value );
-	
-	/**
-	 * Create a new Key for a specific User
-	 * @param userName The ID of the User that will own the new Key
-	 * @param keyId The ID of the new Key
-	 * @param keyValue The value of the new Key
-	 * @return The current state of the newly-created Key
-	 */
-	public Key createKey( String userName, String keyId, String keyValue );
-
-	/**
-	 * Delete a Key
-	 * @param userName The ID of the User that "owns" the Key
-	 * @param keyId The ID of the Key to delete
-	 * @return The current state of the User
-	 */
-	public User deleteKey( String userName, String keyId );
+	public List< User > findAllUsers();
 	
 	/**
 	 * Get and increment the current value of a named persistent counter ("sequence"). If the Sequencer does not
@@ -84,9 +63,37 @@ public interface PersistenceService {
 	public long getCount( String seqName );
 	
 	/**
-	 * Delete a Sequencer from the database
-	 * @param seqName The name of the Sequence
+	 * Convenience method to obtain key value by ID rather than getting entire object
+	 * @param keyId The ID of the Key to obtain
+	 * @return The value of the requested Key
 	 */
-	public void deleteSequencer( String seqName );
+	public String getKeyValueById( String keyId );
 
+	/**
+	 * Get the "master" Key from the database
+	 * @return The master Key
+	 */
+	public Key getMasterKey();
+	
+	/**
+	 * Get a specific User by username
+	 * @param userName The username of the User to obtain
+	 * @return The User with the matching username
+	 */
+	public User getUserByUsername( String userName );
+	
+	/**
+	 * Set the "master" Key in the database (there can only be one master key at a time)
+	 * @param value The value of the Key
+	 * @return The new master Key
+	 */
+	public Key setMasterKey( String value );
+	
+	/**
+	 * Convenience method for saving User changes (uses same method as "createUser")
+	 * @param user The User to update
+	 * @return The current state of the User
+	 */
+	public User updateUser( User user );
+	
 }
--- Server/complete/src/main/java/springboot/PersistenceServiceImpl.java
+++ Server/complete/src/main/java/springboot/PersistenceServiceImpl.java
@@ -17,48 +17,6 @@ public class PersistenceServiceImpl implements PersistenceService {
 	private UserDao userDao;
 	
 	@Override
-	public User createUser( User newUser ) {
-		return userDao.save( newUser );
-	}
-
-	@Override
-	public User getUserByUsername( String userName ) {
-		return userDao.findById( userName ).orElse( null );
-	}
-
-	@Override
-	public User updateUser( User user ) {
-		return userDao.save( user );
-	}
-
-	@Override
-	public void deleteUser( User user ) {
-		userDao.delete( user );
-		
-	}
-
-	@Override
-	public List< User > findAllUsers() {
-		return userDao.findAll();
-	}
-
-	@Override
-	public Key getMasterKey() {
-		return keyDao.findById( Key.MASTER_KEY_ID ).orElse( null );
-	}
-
-	@Override
-	public Key setMasterKey( String value ) {
-		
-		Key newMasterKey = new Key();
-		newMasterKey.setKeyId( Key.MASTER_KEY_ID );
-		newMasterKey.setValue( value );
-		
-		return keyDao.save( newMasterKey );
-		
-	}
-
-	@Override
 	public Key createKey(String userName, String keyId, String keyValue) {
 		
 		Key newKey = new Key();
@@ -73,6 +31,24 @@ public class PersistenceServiceImpl implements PersistenceService {
 
 		return newKey;
 		
+	}
+
+	@Override
+	public User createUser( String userName, String passwordHash ) {
+		
+		// create a new User object given the supplied params
+		User newUser = new User();
+		newUser.setUserName( userName );
+		newUser.setPasswordHash( passwordHash );
+		
+		// persist
+		return createUser( newUser );
+		
+	}
+
+	@Override
+	public User createUser( User newUser ) {
+		return userDao.save( newUser );
 	}
 
 	@Override
@@ -104,16 +80,29 @@ public class PersistenceServiceImpl implements PersistenceService {
 	}
 
 	@Override
-	public User createUser( String userName, String passwordHash ) {
+	public void deleteSequencer(String seqName) {
 		
-		// create a new User object given the supplied params
-		User newUser = new User();
-		newUser.setUserName( userName );
-		newUser.setPasswordHash( passwordHash );
+		// get sequence
+		Sequence sequence = sequenceDao.findById( seqName ).orElse( null );
+
+		// exists? delete it if it does
+		if ( sequence != null ) {
 		
-		// persist
-		return createUser( newUser );
+			sequenceDao.delete( sequence );
+			
+		}
 		
+	}
+
+	@Override
+	public void deleteUser( User user ) {
+		userDao.delete( user );
+		
+	}
+
+	@Override
+	public List< User > findAllUsers() {
+		return userDao.findAll();
 	}
 
 	@Override
@@ -144,18 +133,47 @@ public class PersistenceServiceImpl implements PersistenceService {
 	}
 
 	@Override
-	public void deleteSequencer(String seqName) {
+	public String getKeyValueById(String keyId) {
 		
-		// get sequence
-		Sequence sequence = sequenceDao.findById( seqName ).orElse( null );
-
-		// exists? delete it if it does
-		if ( sequence != null ) {
-		
-			sequenceDao.delete( sequence );
+		// I'm sure there is a better way to query Mongo for this, but this is quick and simple
+		for ( User user : userDao.findAll() ) {
+	
+			// is the specified key ID associated with this user?
+			for ( Key key : user.getKeys() ) {
+				if ( key.getKeyId().equalsIgnoreCase( keyId )) return key.getValue();
+			}
 			
 		}
 		
+		// key wasn't found
+		return null;
+		
+	}
+
+	@Override
+	public Key getMasterKey() {
+		return keyDao.findById( Key.MASTER_KEY_ID ).orElse( null );
+	}
+
+	@Override
+	public User getUserByUsername( String userName ) {
+		return userDao.findById( userName ).orElse( null );
+	}
+
+	@Override
+	public Key setMasterKey( String value ) {
+		
+		Key newMasterKey = new Key();
+		newMasterKey.setKeyId( Key.MASTER_KEY_ID );
+		newMasterKey.setValue( value );
+		
+		return keyDao.save( newMasterKey );
+		
+	}
+
+	@Override
+	public User updateUser( User user ) {
+		return userDao.save( user );
 	}
 	
 }
